cdc855dfa7363c6e5a275d99607dc105b0591d98
Nicolas <nzparra@gmail.com>
2020-03-13 13:22:13 -0500

    [Update]Binary levelorder Header

--- binary_trees.h
+++ binary_trees.h
@@ -46,4 +46,5 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 avl_t *sorted_array_to_avl(int *array, size_t size);
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 #endif
